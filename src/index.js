615e9db338325e3c48744a8ddd72073a4b1a4d5c
larsmarginet <lars.marginet@student.howest.be>
2019-11-28 22:28:54 +0100

    js form validation close #21

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ require('./style.css');
 {
   const init = () => {
     document.documentElement.classList.add('has-js');
+
+    const $form = document.querySelector(`.activity__form`);
+
+    if ($form) {
+      $form.noValidate = true;
+      $form.addEventListener('submit', handleSubmitForm);
+      const fields = $form.querySelectorAll(`.input`);
+      addValidationListeners(fields);
+    }
+
     form();
   };
 
@@ -20,8 +30,6 @@ require('./style.css');
     e.preventDefault();
 
     const $id = parseFloat(e.currentTarget.dataset.id);
-
-    //validation!
 
     //Sets the current page invisble
     const $currentPhase = document.querySelector(`.phase${$id}`);
@@ -44,6 +52,71 @@ require('./style.css');
     //Sets the previous page visible
     const $prevPhase = document.querySelector(`.phase${$id - 1}`);
     $prevPhase.style.display = 'block';
+  };
+
+  const handleSubmitForm = e => {
+    const $form = e.currentTarget;
+    if (!$form.checkValidity()) {
+      e.preventDefault();
+
+      const fields = $form.querySelectorAll(`.input`);
+      fields.forEach(showValidationInfo);
+
+      $form.querySelector(`.jserror`).innerHTML = `Some errors occured`;
+    } else {
+      console.log(`Form is valid => submit form`);
+    }
+  };
+
+  const showValidationInfo = $field => {
+    let message;
+    if ($field.validity.valueMissing) {
+      message = `Required`;
+    }
+    if ($field.validity.typeMismatch) {
+      message = `Type not right`;
+    }
+    if ($field.validity.rangeOverflow) {
+      const max = $field.getAttribute(`max`);
+      message = `Too big, max ${max}`;
+    }
+    if ($field.validity.rangeUnderflow) {
+      const min = $field.getAttribute(`min`);
+      message = `Too small, min ${min}`;
+    }
+    if ($field.validity.tooShort) {
+      const min = $field.getAttribute(`minlength`);
+      message = `Too short, minimum length is ${min}`;
+    }
+    if ($field.validity.tooLong) {
+      const max = $field.getAttribute(`maxlength`);
+      message = `Too long, maximum length is ${max}`;
+    }
+    if (message) {
+      $field.parentElement.querySelector(`.jserror`).textContent = message;
+    }
+  };
+
+  const handeInputField = e => {
+    const $field = e.currentTarget;
+    if ($field.checkValidity()) {
+      $field.parentElement.querySelector(`.jserror`).textContent = ``;
+      if ($field.form.checkValidity()) {
+        $field.form.querySelector(`.jserror`).innerHTML = ``;
+      }
+    }
+  };
+
+  const handeBlurField = e => {
+    const $field = e.currentTarget;
+    showValidationInfo($field);
+  };
+
+  const addValidationListeners = fields => {
+    fields.forEach($field => {
+      $field.addEventListener(`input`, handeInputField);
+      $field.addEventListener(`blur`, handeBlurField);
+    });
   };
 
   init();
--- src/view/activities/activity.php
+++ src/view/activities/activity.php
@@ -2,19 +2,20 @@
   <h2 class="Activity__title">Add an activity!</h2>
   <form class="activity__form" id="insertActivityForm" method="post" action="index.php?page=activity&id=<?php echo $workout_id; ?>&intensity=<?php echo $intensity; ?>">
   <input type="hidden" name="action" value="insertActivity"/>
-
+  <p class="jserror"></p>
     <div class="phase1">
       <!-- First div is for the different phases in Javascript. -->
       <div class="activity__form__section">
         <!-- The second div is for styling. Because in JS you go from display none to display block. So you can't use flexbox or grid -->
         <label class="activity__label title" for="title">Title</label>
         <p class="activity__form__description">Give your activity a short title. E.g. Go run!</p>
-
-        <input class="input__title" id="title" name="title" type="text">
-
+        <div>
+          <input class="input" id="title" name="title" type="text" required>
+          <p class="jserror"></p>
+        </div>
         <label class="activity__label title" for="description">Description</label>
         <p class="activity__form__description">Explain what you have to do. E.g. The steps you have to do.</p>
-        <textarea id="description" name="description" type="text" class="input__description"></textarea>
+        <textarea id="description" name="description" type="text" class="input__description input" required></textarea>
 
         <div class="button__wrapper">
           <a class="activity__button" href="index.php?page=workout"><p class="back-button">Back</p></a>
@@ -31,16 +32,19 @@
         <div class="num__wrapper">
           <div class="num__wrapper-label">
             <label class="activity__label title" for="time1">Minutes</label>
-            <input id="time1" min="0" max="60" name="min" type="number" placeholder="00">
+            <input class="input" id="time1" min="0" max="60" name="min" type="number" value="00" required>
+            <p class="jserror"></p>
           </div>
           <div class="num__wrapper-label">
             <label class="activity__label title" for="time2">Seconds</label>
-            <input id="time2" min="0" max="60" step="15" name="sec" type="number" placeholder="00">
+            <input class="input" id="time2" min="0" max="60" step="15" name="sec" type="number" value="00" required>
+            <p class="jserror"></p>
           </div>
         </div>
         <label class="activity__label title" for="amount">Amount</label>
         <div class="num__wrapper">
-          <input id="amount" name="amount" min="0" max="10" type="number" placeholder="00">
+          <input class="input" id="amount" name="amount" min="0" max="10" type="number" value="01" required>
+          <p class="jserror"></p>
         </div>
         <div class="button__wrapper">
           <button data-id="2"  class="activity__button prev"><p class="back-button">Back</p></button>
@@ -52,23 +56,28 @@
     <div class="phase3">
       <div class="activity__form__section">
         <label class="activity__label title" for="youtube">Youtube link</label>
-        <input class="input__title" id="youtube" name="youtube" type="text">
+        <div>
+          <input class="input" id="youtube" name="youtube" type="url" required>
+          <p class="jserror"></p>
+        </div>
         <p class="activity__label title">Time</p>
         <p class="activity__form__description">From what timestamp do you want the video to start?</p>
         <div class="num__wrapper">
           <div class="num__wrapper-label">
             <label class="activity__label title" for="minute">Minutes</label>
-            <input id="minute" min="0" max="60" name="minute" type="number" placeholder="00">
+            <input class="input" id="minute" min="0" max="60" name="minute" type="number" value="00" required>
+            <p class="jserror"></p>
           </div>
           <div class="num__wrapper-label">
             <label class="activity__label title" for="second">Seconds</label>
-            <input id="second" min="0" max="60" name="second" type="number" placeholder="00">
+            <input class="input" id="second" min="0" max="60" name="second" type="number" value="00" required>
+            <p class="jserror"></p>
           </div>
         </div>
       </div>
       <div class="button__wrapper">
         <button data-id="3"  class="activity__button prev"><p class="back-button">Back</p></button>
-        <button type="submit" class="activity__button"><p class="next-button">Finish</p></button>
+        <input type="submit" class="activity__button next-button" value="finish">
       </div>
     <div>
 
